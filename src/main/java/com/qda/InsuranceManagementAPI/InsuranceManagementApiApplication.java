e267a52fe9a718e0d972d9a2f0ef63c847aa2359
katlegoprince1998 <princekatlego357@gmail.com>
2025-07-03 18:35:52 +0200

    Java Project SetUp

---
+++ src/main/java/com/qda/InsuranceManagementAPI/InsuranceManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.qda.InsuranceManagementAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qda/InsuranceManagementAPI/InsuranceManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qda.InsuranceManagementAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
